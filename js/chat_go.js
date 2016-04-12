81a72702c22bc5ece5a14728e6caffebdd359531
Eduardo Ismael <eduardo78d@gmail.com>
2016-04-12 12:38:46 -0500

    Chat con Go

--- js/chat_go.js
+++ js/chat_go.js
@@ -1,3 +1,17 @@
 $( document ).ready(function() {
-    alert("Hola mundo");
+	
+	var conexion =new WebSocket('ws://localhost:8000/ws');
+	conexion.onopen = function(){
+	conexion.onmessage = function(response){
+			val = $("#chat_area").val();
+    		$("#chat_area").val(val + "\n" + response.data); 
+		}
+	}
+
+    $('#form_message').on('submit', function(e) {
+    	e.preventDefault();
+    	conexion.send($('#msg').val());
+    	$('#msg').val("")
+
+    });
 });
--- server.go
+++ server.go
@@ -3,30 +3,78 @@ package main
 /*
 	export GOPATH=$HOME/work
 	Instalación 
-	go get github.com/gorilla/websocket
+    go get github.com/gorilla/mux
+    go get github.com/gorilla/websocket
 */
 import (
     "log"
     "net/http"
-
     "github.com/gorilla/mux"
+    "github.com/gorilla/websocket"
+    "sync"
 )
 
+var Users = make(map[User]string)
+var UsersRWMutex sync.RWMutex
+
+type User struct {
+    websocket *websocket.Conn
+}
+
 func main() {
-    r := mux.NewRouter()
+    mux := mux.NewRouter()
     cssHandler := http.FileServer(http.Dir("./css/"))
     jsHandler := http.FileServer(http.Dir("./js/"))
+    
+    mux.HandleFunc("/", HomeHandler).Methods("GET")
+    mux.HandleFunc("/ws", web_socket)
 
+    http.Handle("/", mux)
     http.Handle("/css/", http.StripPrefix("/css/", cssHandler))
     http.Handle("/js/", http.StripPrefix("/js/", jsHandler))
 
-    r.HandleFunc("/", HomeHandler)
-    
-    http.Handle("/", r)
     log.Println("Server running on :8000")
     log.Fatal(http.ListenAndServe(":8000", nil))
 }
 
-func HomeHandler(rw http.ResponseWriter, r *http.Request) {
-    http.ServeFile(rw, r, "index.html")
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+    http.ServeFile(w, r, "index.html")
+}
+
+func web_socket(w http.ResponseWriter, r *http.Request){
+    ws, err := websocket.Upgrade(w, r, nil, 1024, 1024)
+    if err != nil {
+        log.Println(err)
+        return
+    }
+    user := create_user(ws, "Eduardo")
+    add_user(user)
+    for{
+        messageType, message, err := ws.ReadMessage()
+        if err != nil {
+            return
+        }
+        send_echo(messageType, message)
+    }
+}
+
+func create_user(ws *websocket.Conn, usuario string) User{
+    return User{ websocket: ws}
+}
+
+func add_user(user User){
+    UsersRWMutex.Lock()
+    defer UsersRWMutex.Unlock()
+    Users[user] = ""
+}
+
+func send_echo(messageType int, message []byte) {
+    UsersRWMutex.RLock()
+    defer UsersRWMutex.RUnlock()
+    log.Println("To send messages")
+    for user, _ := range Users {
+        if err := user.websocket.WriteMessage(messageType, message); err != nil {
+            return
+        }
+    }
 }
